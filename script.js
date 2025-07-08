f0888db6400e32b6d83ad0206ab00ad5203ff4db
Ibrohimovm1 <muhammahammadamin@gmail.com>
2025-07-08 11:34:57 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let body  = document.querySelector('body')
+let p = document.querySelector('p')
+let pcolor = prompt('text rangini tanlang')
+let bgBody = prompt('body rangini tanlang')
+
+
+body.style.backgroundColor = bgBody
+p.style.color = pcolor
